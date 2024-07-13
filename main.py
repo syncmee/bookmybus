70fc3c0be32d14b28448a8b4a164942644ddc773
syncmee <129711670+syncmee@users.noreply.github.com>
2024-07-13 22:33:28 +0530

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Dummy data for user authentication
+# authentication
 users = {
     'testuser': 'password123'
 }
