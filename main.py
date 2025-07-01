dc68a3733ebed6a749f6baa7b7ee876734f1fd8b
Rohit Talukdar <129711670+syncmee@users.noreply.github.com>
2025-07-02 03:37:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -768,9 +768,7 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables
     app.run(debug=True)
